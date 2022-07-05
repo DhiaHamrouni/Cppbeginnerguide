aef3dc2346a24cea6ef347aaae89197c3a537e1b
Dhia Hamrouni <90195261+DhiaHamrouni@users.noreply.github.com>
2022-07-05 15:56:06 +0100

    Add files via upload

---
+++ adventure/hazaah.cpp
@@ -0,0 +1,197 @@
+#include<iostream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+void Human()
+{
+    int seconds=1;
+    int count=0;
+    char ch1;
+    char ch2;
+    while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+    cout<<"it's a tuesday morning , the sun shining high and bright over the street of Shapolis. You wake up in your appartement with a throbbing headache and you feel very dizzy. You turn around your bed and see a naked cat-lady sleeping next to you! What do you do ? \n\n";
+    while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+    cout<<"A) slowly get out of bed and try not to wake her up \nB) Go back to sleep and wish it was all a dream. \nC) wake her up and ask who she is?\n";
+    cin>>ch1;
+    if (ch1=='a' || ch1=='A')
+{
+    
+    cout<<"As you're thinking how to get out of this situation you're in , you remember that you come from a family that have somewhat of a little rigid opinion considering inter-species intercourse.\n";
+    while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+    cout<<"You try to move around as quietly and slowly as possible to get out of bed. As you're too focused on getting out , you hear a whisper in your ear : 'Where are you going Tiger??' you look up and there she is all awake looking right back at you.\n";
+    while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+    cout<<"What will you do?\n";
+    while(count!=2)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+    cout<<"A)You don't quite answer the question and just keep staring at her.\nB)you say that you were just getting up and did not want to wake her up.\n";
+    cin>>ch2;
+    if (ch2=='a' || ch2=='A')
+    {
+        cout<<"you keep staring at her with your eyes wide open and you don't say a word. A look of concern draws on her face and she asks you : ''You okay there buddy?you seem a little of your edge. Maybe you need a round two to wake up 'wink wink' '' \n";
+        while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+        cout<<"you feel a little pride build up inside of you as she said that but then you remember that you are still in bed with this cat lady, you jump out of the bed while covering yourself and you run to the bathroom\n";
+        while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+        cout<<"What do you do next ??\n";
+        cout<<"A)You try to comprehend the situation andfigure out a plan to get her out.\nB)Take a panic shower.\n";
+    }
+    else if (ch2=='b' || ch2=='B')
+    {
+
+    }
+}
+else if (ch1=='b' || ch1=='B')
+{
+    
+}
+else if (ch1=='c' || ch1=='C')
+{
+
+}
+
+}
+int main()
+{
+int seconds=1;
+int count=0;
+char choice;
+cout << "Hello , welcome to this tiny Adventure that I created. \n";
+    while(count!=3)
+    {
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+cout << "In this Universe , You can be whatever you want. You can be a doggo , a cat , an astronaut or an alien whatever you want. \n";
+    while(count!=3){
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+cout << "Let me set in the univers for you doe. You live on an earth inhabited by different species. There are humanoid animals with actual jobs and social statues and where aliens frequent often for pit stops or live there \n " ;
+    while(count!=10){
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+cout << "Alright , now arm up with your imagination and imagine this world, take your time.Here you go 10 seconds because am such a good person :3 . \n";
+    while(count!=10){
+        cout << seconds<<"\n";
+        Sleep(1000);
+        seconds++;
+        count++;
+    }
+    count=0;
+cout << "Good , now , let's create your character: \n";
+cout << "Choose What you want to be : \n ";
+    while(count!=3){
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+cout << "A) Human \n B) Doggo \n C) Cat \n D) Alien \n E) Fox \n";
+    while(count!=3){
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+cout << "Before you choose , let me give you some specific of each race :\n\n";
+    while(count!=3){
+        Sleep(1000);
+        seconds++;
+        count++;
+        seconds=1;
+    }
+    count=0;
+cout << "Humans : Armed with a superior intelect and hand craft more than any other species on earth , Humans imposed themselves on the planet.They can also access many different paths in life and become basically anything they want to be. They are limited tho in physical abilities and have some what of narrowed vision towards other races and unknown creatures. They are also easily mimicked by species that can disguise efficiently and have highly deceitful such as foxes and aliens making them affect their social statues which is imperative for the humans.\n\nDoggo: This race is highly known for their pride and trustworth. Whether it is in a fight or daily routines , a doggo's faith is unquestionable during 'ruff' times. Even though they have a lower iq than most humans, they excell in detecting scents and defending themselves thanks to their good offensive stats and will power. the good doggos work in helping in need humans or to handle police activites and guarding high value. It is aso noted that humans and doggos have a chemistry between them and usually get along pretty well unlike doggos and cats/foxes \n\nCats: They excell in appearances and fluffiness , cats make for great snuggling partners and casual one night stands if you know what I mean. They have some what of a fiery relationship with dogs but whenraised together , dogs and cats get along well so don't be suprised if you see a dog and a cat as a couple. Cats get along pretty well with humans. They tend to be more agressive when provoked or when they are in a bad mood even with their companions. Their appearances make for great social scores which advantages then in certain domains such as modeling and managing sales.\n\nAliens: As the name suggests , these creatures are not originally from earth.Their existance within the earth is kind of Hush Hush so it's best not to reveal that you're not an alien or else you will be probably followed around by authorities. They have somewhat of a superiority complex since they are the only species to have a higher intellect than the humans so they are not very accpeted in social gatherings. They excel in scientific fields and innovations but have low social interaction with other species stats but they communicate better with other aliens. As stated before , Aliens have the capacity to disguise themselves as other entities so they can be pretty hard to detect unless you are faced with a paranoid humans and doggos with high scent stats then you are at risk.\n\nFoxes: Foxes are the embodiment of the syaing 'looks can be deceiving', they have great wit and sleight of hand so they make a living off other people the easy way. The main problem is that this life is kinda hard and makes them a permanent target of authorities especially since they have their eternal enemy doggos working that part of the world.\n";
+cout << "Make your choice : ";
+cin >> choice;
+/*while (choice!='a' || choice!='A' || choice!='b' || choice!='B' || choice!='c' || choice!='C' || choice!='d' || choice!='D' || choice!='e' || choice!='E')
+{
+    cout << "Make your choice,make sure it's a , b , c , d or e: ";
+    cin >> choice; 
+}
+*/
+if (choice=='a' || choice=='A')
+{
+    cout <<"congratualtions , you picked Human . Interesting , boring , but interesting.\n\n";
+    Human();
+}
+else if (choice=='b' || choice=='B')
+{
+    cout <<"congratualtions , you picked Doggo . It's all about that Pug life, right?\n";
+}
+else if (choice=='c' || choice=='C')
+{
+    cout <<"congratualtions , you picked Cat . This opens up a lot of paw-ssibilities \n";
+}
+else if (choice=='d' || choice=='D')
+{
+    cout <<"congratualtions , you picked Alien . Be careful not to bring any stardust with you alright ! \n";
+}
+else if (choice=='e' || choice=='E')
+{
+    cout <<"congratualtions , you picked Fox . Good but remember, you have to think outside the fox! \n";
+}
+else
+{
+    cout << " you picked wrong , you gotta restart the whole thing now!";
+}
+}
